ecf2344c4b6f49d9c325ae470714dd07b0bd1566
Jason Stinson <jsfeb26@gmail.com>
2023-03-26 15:35:57 -0500

    Quickly allow user to create post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,11 +7,16 @@ import relativeTime from "dayjs/plugin/relativeTime";
 import { api, type RouterOutputs } from "~/utils/api";
 import Image from "next/image";
 import { LoadingPage } from "~/components/Loading";
+import { useState } from "react";
 
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
   const { user } = useUser();
+
+  const [input, setInput] = useState("");
+
+  const { mutate } = api.posts.create.useMutation();
 
   if (!user) return null;
 
@@ -27,7 +32,11 @@ const CreatePostWizard = () => {
       <input
         placeholder="Type some emojis!"
         className="grow bg-transparent outline-none"
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
       />
+      <button onClick={() => mutate({ content: input })}>Post</button>
     </div>
   );
 };
