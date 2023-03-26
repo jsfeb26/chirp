4c2114348af20c75615b2fdf6b75d4410f7f194b
Jason Stinson <jsfeb26@gmail.com>
2023-03-26 17:31:24 -0500

    Build out profile page styling and layout

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -3,19 +3,19 @@ import Head from "next/head";
 import { createProxySSGHelpers } from "@trpc/react-query/ssg";
 import superjson from "superjson";
 
-import { LoadingPage } from "~/components/Loading";
 import { api } from "~/utils/api";
 import { appRouter } from "~/server/api/root";
 import { prisma } from "~/server/db";
+import { PageLayout } from "~/components/Layout";
+import Image from "next/image";
 
-const ProfilePage: NextPage = () => {
-  const { data, isLoading } = api.profile.getUserByUsername.useQuery({
-    username: "jsfeb26",
+// TODO: This is not working for some reason so it's not showing username
+// type PageProps = InferGetStaticPropsType<typeof getStaticProps>;
+
+const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
+  const { data } = api.profile.getUserByUsername.useQuery({
+    username,
   });
-
-  if (isLoading) {
-    return <LoadingPage />;
-  }
 
   if (!data) {
     return <div>404</div>;
@@ -24,11 +24,24 @@ const ProfilePage: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Profile</title>
+        <title>{data.username}</title>
       </Head>
-      <main className="flex h-screen justify-center">
-        <div>{data.username}</div>
-      </main>
+      <PageLayout>
+        <div className="relative h-36 bg-slate-600">
+          <Image
+            src={data.profileImageUrl}
+            alt={`${data.username ?? ""}'s profile pic`}
+            width={128}
+            height={128}
+            className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black bg-black"
+          />
+        </div>
+        <div className="h-[64px]" />
+        <div className="p-4 text-2xl font-bold">{`@${
+          data.username ?? ""
+        }`}</div>
+        <div className="w-full border-b border-slate-400" />
+      </PageLayout>
     </>
   );
 };
