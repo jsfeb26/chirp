2f8c347fbc22b3948e18b37a3aeaf97304bad505
Jason Stinson <jsfeb26@gmail.com>
2023-03-26 16:45:41 -0500

    Display username in profile page using new profile router

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -1,14 +1,29 @@
 import { type NextPage } from "next";
 import Head from "next/head";
 
+import { LoadingPage } from "~/components/Loading";
+import { api } from "~/utils/api";
+
 const ProfilePage: NextPage = () => {
+  const { data, isLoading } = api.profile.getUserByUsername.useQuery({
+    username: "jsfeb26",
+  });
+
+  if (isLoading) {
+    return <LoadingPage />;
+  }
+
+  if (!data) {
+    return <div>404</div>;
+  }
+
   return (
     <>
       <Head>
         <title>Profile</title>
       </Head>
       <main className="flex h-screen justify-center">
-        <div>Profile View</div>
+        <div>{data.username}</div>
       </main>
     </>
   );
