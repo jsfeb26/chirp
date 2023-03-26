efaccd2d221936daf27354da80ae2736406f14eb
Jason Stinson <jsfeb26@gmail.com>
2023-03-26 16:45:13 -0500

    Add profile router to get user

---
+++ src/server/api/routers/profile.ts
@@ -0,0 +1,26 @@
+import { clerkClient } from "@clerk/nextjs/server";
+import { TRPCError } from "@trpc/server";
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { filterUserForClient } from "~/server/helpers/filterUserForClient";
+
+// This runs on the server
+export const profileRouter = createTRPCRouter({
+  getUserByUsername: publicProcedure
+    .input(z.object({ username: z.string() }))
+    .query(async ({ input }) => {
+      const [user] = await clerkClient.users.getUserList({
+        username: [input.username],
+      });
+
+      if (!user) {
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "User note found",
+        });
+      }
+
+      return filterUserForClient(user);
+    }),
+});
