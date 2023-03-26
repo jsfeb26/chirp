06ed28a1c824741fed6b6f77aef6fc6d5f742974
Jason Stinson <jsfeb26@gmail.com>
2023-03-26 18:01:30 -0500

    Display single post in PostPage

--- src/pages/post/[id].tsx
+++ src/pages/post/[id].tsx
@@ -1,17 +1,55 @@
-import { type NextPage } from "next";
+import { type GetStaticProps, type NextPage } from "next";
 import Head from "next/head";
 
-const PostPage: NextPage = () => {
+import { api } from "~/utils/api";
+import { PageLayout } from "~/components/Layout";
+import { PostView } from "~/components/PostView";
+import { generateSSGHelper } from "~/server/helpers/ssgHelper";
+
+// TODO: This is not working for some reason so it's not showing username
+// type PageProps = InferGetStaticPropsType<typeof getStaticProps>;
+
+const PostPage: NextPage<{ id: string }> = ({ id }) => {
+  const { data } = api.posts.getById.useQuery({
+    id,
+  });
+
+  if (!data) {
+    return <div>404</div>;
+  }
+
   return (
     <>
       <Head>
-        <title>Post</title>
+        <title>{`${data.post.content} - ${data.author.username}`}</title>
       </Head>
-      <main className="flex h-screen justify-center">
-        <div>Post View</div>
-      </main>
+      <PageLayout>
+        <PostView {...data} />
+      </PageLayout>
     </>
   );
+};
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const ssg = generateSSGHelper();
+
+  const id = context.params?.id;
+
+  // TODO: Redirect to a different page
+  if (typeof id !== "string") throw new Error("no slug");
+
+  await ssg.posts.getById.prefetch({ id });
+
+  return {
+    props: {
+      trpcState: ssg.dehydrate(),
+      id,
+    },
+  };
+};
+
+export const getStaticPaths = () => {
+  return { paths: [], fallback: "blocking" };
 };
 
 export default PostPage;
