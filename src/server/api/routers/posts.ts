65c64f7b98fd825b7ad419c053985b3a673ac7fd
Jason Stinson <jsfeb26@gmail.com>
2023-03-26 15:35:33 -0500

    Add protected create post route

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -3,7 +3,12 @@
 import type { User } from "@clerk/nextjs/dist/api";
 import { clerkClient } from "@clerk/nextjs/server";
 import { TRPCError } from "@trpc/server";
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { z } from "zod";
+import {
+  createTRPCRouter,
+  privateProcedure,
+  publicProcedure,
+} from "~/server/api/trpc";
 
 const filterUserForClient = (user: User) => {
   return {
@@ -16,7 +21,10 @@ const filterUserForClient = (user: User) => {
 // This runs on the server
 export const postsRouter = createTRPCRouter({
   getAll: publicProcedure.query(async ({ ctx }) => {
-    const posts = await ctx.prisma.post.findMany({ take: 100 });
+    const posts = await ctx.prisma.post.findMany({
+      take: 100,
+      orderBy: [{ createdAt: "desc" }],
+    });
 
     const users = (
       await clerkClient.users.getUserList({
@@ -44,4 +52,26 @@ export const postsRouter = createTRPCRouter({
       };
     });
   }),
+
+  create: privateProcedure
+    .input(
+      z.object({
+        content: z.string().emoji().min(1).max(280),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const authorId = ctx.userId;
+
+      const post = await ctx.prisma.post.create({
+        data: {
+          authorId,
+          // We know this will always be here because zod on the input above
+          // If it's not then TRPC will throw the error for us and the user
+          // can't actually call this without passing the validation
+          content: input.content,
+        },
+      });
+
+      return post;
+    }),
 });
