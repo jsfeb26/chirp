abe7db723bec84ee99c82d207740e8340cdfd282
Jason Stinson <jsfeb26@gmail.com>
2023-03-26 14:29:10 -0500

    Switch to next/image in components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,7 @@ import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
 
 import { api, type RouterOutputs } from "~/utils/api";
+import Image from "next/image";
 
 dayjs.extend(relativeTime);
 
@@ -15,11 +16,12 @@ const CreatePostWizard = () => {
 
   return (
     <div className="flex w-full gap-3">
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img
+      <Image
         src={user.profileImageUrl}
         alt="Profile image"
         className="h-14 w-14 rounded-full"
+        width={56}
+        height={56}
       />
       <input
         placeholder="Type some emojis!"
@@ -34,7 +36,13 @@ type PostWithUser = RouterOutputs["posts"]["getAll"][number];
 const PostView = ({ post, author }: PostWithUser) => {
   return (
     <div key={post.id} className="flex gap-3 border-b border-slate-400 p-4">
-      <img src={author.profileImageUrl} className="h-14 w-14 rounded-full" />
+      <Image
+        src={author.profileImageUrl}
+        alt={`@${author.username}'s profile picture`}
+        className="h-14 w-14 rounded-full"
+        width={56}
+        height={56}
+      />
       <div className="flex flex-col">
         <div className="flex gap-1 text-slate-300">
           <span>{`@${author.username}`}</span>
